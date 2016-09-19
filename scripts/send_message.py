854566d2fd5d904b0f5c2e54d9cbecb035d688fb
Kent Shikama <kent@kentshikama.com>
2016-09-19 03:01:40 -0700

    Remove send_message.py script

--- scripts/send_message.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-connection = socket.create_connection(('127.0.0.1',1024)) # Assuming CS server on localhost port 1024
-connection.sendall(b'kent\x00harry\x00test\x00')
-response = connection.makefile()
-print(response.readline())
